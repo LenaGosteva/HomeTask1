fcb70ed05dbaebdbb2bbe92d4c1041e231910d10
LenaGosteva <114250252+LenaGosteva@users.noreply.github.com>
2022-09-24 19:38:47 +0300

    finish

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int i = in.nextInt(), sum = i/100 + (i/10)%10 + i % 10;
-        System.out.println(sum + " " + (i + 2 - i%2));
+        int a = in.nextInt(), b = in.nextInt(), n = in.nextInt(), sum = n*(a*100+ b);
+        System.out.println(sum /100 + " " + sum%100);
     }
 }
