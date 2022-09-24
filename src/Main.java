6a13c1f485db6af942cb74ba9ce5fae5924e6a58
LenaGosteva <114250252+LenaGosteva@users.noreply.github.com>
2022-09-24 19:36:13 +0300

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        int i = in.nextInt(), sum = i/100 + (i/10)%10 + i % 10;
+        System.out.println(sum + " " + (i + 2 - i%2));
     }
 }
