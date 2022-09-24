790dc85fe643f94d1396b73bdad449730edfbd29
LenaGosteva <114250252+LenaGosteva@users.noreply.github.com>
2022-09-24 20:03:08 +0300

    finish

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Double x = in.nextDouble();
-        if ((x>=-2.0 && x <=3.0) || (x >= 6.0 && x <= 9.0)){
-            System.out.println("false");
+        int a = in.nextInt(), b = in.nextInt(), c = in.nextInt();
+        if(((100 <= a && 999>=a)|| (-100 >= a && -999<=a))&&(100 <= b && 999>=b)|| (-100 >= b && -999<=b)){
+            if (a % 5 == 0 && b % 5 == 0){
+                System.out.println("true");
+            }
+            else{
+                System.out.println("false");
+            }
         }
-        else{
-            System.out.println("true");
+        else if(((100 <= a && 999>=a)|| (-100 >= a && -999<=a))&&(100 <= c && 999>=c)|| (-100 >= c && -999<=c)){
+            if (a % 5 == 0 && c % 5 == 0){
+                System.out.println("true");
+            }
+            else{
+                System.out.println("false");
+            }
         }
-
+        else if(((100 <= c && 999>=c)|| (-100 >= c && -999<=c))&&(100 <= b && 999>=b)|| (-100 >= b && -999<=b)){
+            if (c % 5 == 0 && b % 5 == 0){
+                System.out.println("true");
+            }
+            else{
+                System.out.println("false");
+            }
+        }
     }
 }
