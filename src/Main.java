f86c5f8528dc358b8fe5f2a0032469329f2ef8eb
LenaGosteva <114250252+LenaGosteva@users.noreply.github.com>
2022-09-24 19:53:54 +0300

    finish

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int x = in.nextInt(), h = x /3600, min = (x%3600)/60, sec = (x%3600)%60;
-        String stmin = String.valueOf(min), stsec = String.valueOf(sec);
-        if (min < 10){
-            stmin = "0" + stmin;
+        Double x = in.nextDouble();
+        if ((x>=-2.0 && x <=3.0) || (x >= 6.0 && x <= 9.0)){
+            System.out.println("false");
         }
-        if (sec < 10){
-            stsec = "0" + stsec;
+        else{
+            System.out.println("true");
         }
-        System.out.println(h + ":" + stmin + ":" + stsec);
+
     }
 }
